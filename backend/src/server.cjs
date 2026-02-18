ccd0aa00eead2f94c6f56c802eadb83d796f9005
Deekshitha <limgaladeekshitha@gmail.com>
2026-02-18 22:43:49 +0530

    prepared for the deployment

--- backend/src/server.cjs
+++ backend/src/server.cjs
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv");
 const cors = require("cors")
+const path = require("path")
 
 const connectDB = require("./config/db")
 const notesRoutes = require("./routes/notesRoutes");
@@ -12,9 +13,13 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors({
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin: "http://localhost:5173"
 }))
+}
+
 app.use(express.json());
 app.use(rateLimiter)
 
@@ -22,6 +27,13 @@ app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../../frontend","dist","index.html"));
+});
+
+}
 
 connectDB().then(()=>{
    app.listen(PORT, ()=>{
